64141b412c46a889b4e28845f87b3b46717b03e5
Michel Metran <michelmetran@gmail.com>
2022-03-14 19:24:45 -0300

    Ajustes

--- src/open_geodata/geo.py
+++ src/open_geodata/geo.py
@@ -4,10 +4,12 @@
 import os
 import geopandas as gpd
 
-try:
-    sp_250k = gpd.read_file(
-        filename=os.path.join(os.path.dirname(__file__), 'data', 'sp_250k_wgs84.geojson'),
-    )
+sp_250k = gpd.read_file(
+    filename=os.path.join(os.path.dirname(__file__), 'data', 'sp_250k_wgs84.geojson'),
+)
 
-except Exception as e:
-    print(e)
+
+
+
+
+
