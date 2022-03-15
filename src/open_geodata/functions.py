18ca9554cc4d9d04be7d23bdffc56ee92285fe5d
Michel Metran <michelmetran@gmail.com>
2022-03-14 23:41:18 -0300

    Ajustes

---
+++ src/open_geodata/functions.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+# Funções para trabalhar com dados geoespaciais
+
+
+
+
+
+def find_neighbors(gdf, column_name):
+    """
+    Encontre os vizinhos e insere coluna "neighbors" no geodataframe com seus nomes
+
+    :param column_name: Título da Coluna que contém o nome do vizinho
+    :param gdf: geodataframe de input
+    :return: geodataframe com uma coluna "neighbors", com o nome dos vizinhos
+    """
+    gdf['neighbors'] = ''
+
+    for index, row in gdf.iterrows():
+        neighbors = gdf[gdf['geometry'].touches(row['geometry'])][column_name].tolist()
+        gdf.at[index, 'neighbors'] = ', '.join(neighbors)
+    return gdf
+
+
+def share_boundary(gdf, gdf_interest):
+    """
+    Adaptação de https://stackoverflow.com/questions/66153429/selecting-polygons-which-shares-a-boundary-with-target-polygon-in-python-prefera
+    :param gdf:
+    :param gdf_interest:
+    :return:
+    """
+    if len(gdf_interest['geometry']) == 1:
+        gdf_interest = gdf_interest['geometry'].values[0]
+    else:
+        print('"gdf_interest" tem mais de um polígono')
+
+    return gdf[gdf.geometry.apply(lambda x: x.touches(gdf_interest))]
+
+
+if __name__ == '__main__':
+    print('pass')
--- src/open_geodata/geo.py
+++ src/open_geodata/geo.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# Funções para carregar dados geoespaciais
+
 import os
+from functions import *
 import geopandas as gpd
 
 
@@ -13,8 +16,20 @@ def load_dataset(name):
     gdf = gpd.read_file(
         filename=os.path.join(os.path.dirname(__file__), 'data', name),
     )
+    gdf.info()
     return gdf
 
 
 if __name__ == '__main__':
-    print('sss')
+    # Lê dados
+    gdf = load_dataset('sp_250k_wgs84.geojson')
+    print(gdf.head())
+
+    # Teste "find_neighbors" attribute table
+    # gdf = find_neighbors(gdf, 'municipio_nome')
+    print(gdf.head())
+
+    # Teste "find_neighbors" spatial
+    gdf_interest = gdf.loc[gdf['id_municipio'] == 3548906]
+    gdf = share_boundary(gdf, gdf_interest)
+    print(gdf.head())
