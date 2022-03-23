0373cc13750dea6d97ff2b6a959f3028b08024e8
GitHub Action <action@github.com>
2022-03-23 18:19:13 +0000

    Update Data using GitAction

--- src/open_geodata/_version.py
+++ src/open_geodata/_version.py
@@ -7,5 +7,5 @@ Provides open_geodata version information.
 
 from incremental import Version
 
-__version__ = Version("open_geodata", 22, 3, 40)
+__version__ = Version("open_geodata", 22, 3, 41)
 __all__ = ["__version__"]
