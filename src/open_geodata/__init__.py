4a61531b8303c9e325741f267b9244fa4a171868
Michel Metran <michelmetran@gmail.com>
2022-06-05 21:07:22 -0300

    Retira dados

--- src/open_geodata/__init__.py
+++ src/open_geodata/__init__.py
@@ -1,12 +1,12 @@
 from open_geodata.functions import share_boundary
-from open_geodata.lyr import base, pd_piracicaba
+from open_geodata.lyr import base
 from open_geodata.folium_plus import adds
 from open_geodata.converts import converts, others
 from open_geodata.sidra import ibge
 
 __all__ = [
     'share_boundary',
-    'base', 'pd_piracicaba',
+    'base',
     'adds',
     'converts',
     'ibge',
