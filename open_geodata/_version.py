00506099c59682c5a925a59afad0d0a1b9c06865
GitHub Action <action@github.com>
2022-11-26 03:29:13 +0000

    Update Version using GitAction

--- open_geodata/_version.py
+++ open_geodata/_version.py
@@ -7,5 +7,5 @@ Provides open_geodata version information.
 
 from incremental import Version
 
-__version__ = Version("open_geodata", 22, 6, 44)
+__version__ = Version("open_geodata", 22, 6, 45)
 __all__ = ["__version__"]
